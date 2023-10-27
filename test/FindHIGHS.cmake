09f03be0cec4627461f6561d2c6312fbf131db03
sebastianopriscan <sebastianroberto.opriscan@gmail.com>
2023-10-27 20:48:31 +0200

    Third try for remote action, this one installs the solver too
    Removed another wrong "*" from the code in FindHIGHS.cmake

--- test/FindHIGHS.cmake
+++ test/FindHIGHS.cmake
@@ -9,15 +9,15 @@ if(WIN32)
 else(WIN32)
         find_path(HIGHS_INCLUDE_DIR Highs.h
                 PATHS
-                ${HIGHS_SOURCE_DIR}/HiGHS/src
-                ${HIGHS_INSTALL_DIR}/include/highs
                 /usr/local/include/highs
+                ${HIGHS_INSTALL_DIR}/include/highs
+                ${HIGHS_SOURCE_DIR}/HiGHS/src
         )
 
         find_library(HIGHS_LIBRARIES NAMES highs
                 PATHS
-                ${HIGHS_SOURCE_DIR}/HiGHS/build/lib
-                ${HIGHS_INSTALL_DIR}*/include/highs/interfaces
                 /usr/local/lib
+                ${HIGHS_INSTALL_DIR}/include/highs/interfaces
+                ${HIGHS_SOURCE_DIR}/HiGHS/build/lib
         )
 endif(WIN32)
